5b202741df84a7bfb79a219c78284db940e4655f
Ankush <ankushvankore@gmail.com>
2025-08-22 11:01:21 +0530

    Date: 22-08-2025

---
+++ MyAutomationProject/src/com/TestNGDemos/D03Before_After_Test.java
@@ -0,0 +1,48 @@
+package com.TestNGDemos;
+
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class D03Before_After_Test {
+	WebDriver driver;
+	
+	@BeforeTest
+	public void launchBrowser()
+	{
+		System.out.println("Before Test");
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+	}
+	@AfterTest
+	public void closeBrowser()
+	{
+		System.out.println("After Test");
+		driver.close();
+	}
+	@Test(priority = 1)
+	public void testMyntra() {
+		driver.get("https://www.myntra.com/");
+		System.out.println("Title: " + driver.getTitle());
+	}
+	@Test(priority = 3)
+	public void testAmazon() {
+		driver.get("https://www.amazon.in/");
+		System.out.println("Title: " + driver.getTitle());
+	}
+	@Test (priority = 2)
+	public void testMeesho() {
+		driver.get("https://www.meesho.com/");
+		System.out.println("Title: " + driver.getTitle());
+	}
+	@Test (priority = 4)
+	public void testFlipkart() {
+		driver.get("https://www.flipkart.com/");
+		System.out.println("Title: " + driver.getTitle());
+	}
+}
---
+++ MyAutomationProject/src/com/TestNGDemos/D04Before_After_Metod.java
@@ -0,0 +1,58 @@
+package com.TestNGDemos;
+
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class D04Before_After_Metod {
+WebDriver driver;
+	
+	@BeforeTest
+	public void launchBrowser()
+	{
+		System.out.println("Before Test");
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+	}
+	@AfterTest
+	public void closeBrowser()
+	{
+		System.out.println("After Test");
+		driver.close();
+	}
+	
+	@BeforeMethod
+	public void beforeMethod()
+	{
+		System.out.println("Before Method");
+	}
+	@AfterMethod
+	public void printTitle()
+	{
+		System.out.println("Title: " + driver.getTitle());
+	}
+	
+	@Test(priority = 1)
+	public void testMyntra() {
+		driver.get("https://www.myntra.com/");
+	}
+	@Test(priority = 3)
+	public void testAmazon() {
+		driver.get("https://www.amazon.in/");
+	}
+	@Test (priority = 2)
+	public void testMeesho() {
+		driver.get("https://www.meesho.com/");
+	}
+	@Test (priority = 4)
+	public void testFlipkart() {
+		driver.get("https://www.flipkart.com/");
+	}
+}
