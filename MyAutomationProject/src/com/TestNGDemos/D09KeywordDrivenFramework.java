a78f125378cb7b90db84fde29e04c0da01fed0d4
Ankush <ankushvankore@gmail.com>
2025-08-26 11:10:37 +0530

    Date: 26 Aug 2025

---
+++ MyAutomationProject/src/com/TestNGDemos/D09KeywordDrivenFramework.java
@@ -0,0 +1,44 @@
+package com.TestNGDemos;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.time.Duration;
+import java.util.Properties;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class D09KeywordDrivenFramework {
+	File file;
+	FileInputStream fis;
+	Properties prop;
+
+	WebDriver driver;
+
+	@Test
+	public void automobileTest() {
+		driver.findElement(By.linkText(prop.getProperty("autoLinkText"))).click();
+		new Select(driver.findElement(By.id(prop.getProperty("makeID")))).selectByIndex(3);
+		driver.findElement(By.xpath(prop.getProperty("engPerXP"))).sendKeys("1000");
+		driver.findElement(By.cssSelector(prop.getProperty("manDateCSS"))).sendKeys("08/25/2025");
+	}
+
+	@BeforeTest
+	public void beforeTest() throws IOException {
+		file = new File("src/com/TestNGDemos/MyLocators.properties");
+		fis = new FileInputStream(file);
+		prop = new Properties();
+		prop.load(fis);	//Read the properties
+		
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+		driver.get(prop.getProperty("url"));
+	}
+}
